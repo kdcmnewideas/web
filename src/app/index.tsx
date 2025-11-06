996a92d2d2cc5017775cd129c521316de0db9945
mohitmadras <mohitmadras@gmail.com>
2025-11-06 22:50:57 +0530

    updated changes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,13 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { AppText } from "@/components/AppText";
 
-export default function Index() {
+export default function IndexScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="justify-center flex-1 p-4">
+      <AppText center>
+        Open up <AppText bold>app/index.tsx</AppText> to start working on your
+        app!
+      </AppText>
     </View>
   );
 }

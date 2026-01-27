428cc31819c8752b8353a7c670cfefded1710e19
mohitmadras <mohitmadras@gmail.com>
2026-01-27 18:43:57 +0530

    updated leaderboard page

--- src/app/features/student/student.routes.ts
+++ src/app/features/student/student.routes.ts
@@ -4,6 +4,12 @@ export const STUDENT_ROUTES: Routes = [
   {
     path: 'home',
     loadComponent: () => import('./student-home/student-home').then((m) => m.StudentHome),
+    title: 'Aile Learning',
+  },
+  {
+    path: 'leaderboard',
+    loadComponent: () => import('./leaderboard/leaderboard').then((m) => m.Leaderboard),
+    title: 'Leaderboard',
   },
   {
     path: '**',

3b01a86899640cfe499e9668cfa11bc1997cf067
mohitmadras <mohitmadras@gmail.com>
2026-02-03 19:59:56 +0530

    subject

--- src/app/features/student/subject/lesson-card/lesson-card.ts
+++ src/app/features/student/subject/lesson-card/lesson-card.ts
@@ -1,7 +1,7 @@
 import { ScreenName } from './../../../../shared/constants/screen-names.constant';
 import { Component, computed, inject, input } from '@angular/core';
 import { Router } from '@angular/router';
-import { Box, CircleCheck, CirclePlay, Clock, Lock, LucideAngularModule, PenTool } from 'lucide-angular';
+import { Box, CircleCheck, CirclePlay, Clock, Lock, LucideAngularModule, PenTool, RotateCw } from 'lucide-angular';
 
 @Component({
   selector: 'app-lesson-card',
@@ -24,7 +24,8 @@ export class LessonCard {
     CirclePlay,
     Clock,
     Box,
-    PenTool
+    PenTool,
+    RotateCw
   };
 
   serialNumber = computed(() => String(this.index() + 1).padStart(2, '0'));

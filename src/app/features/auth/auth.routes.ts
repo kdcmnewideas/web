861abcca37daf2a95e9b988730534befa2452e2f
mohitmadras <mohitmadras@gmail.com>
2026-02-24 13:55:37 +0530

    completed verify-account

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -5,7 +5,8 @@ export const AUTH_ROUTES: Routes = [
     path: 'login',
     loadComponent: () => import('./login/login').then((m) => m.Login),
     title: 'Login',
-  },{
+  },
+  {
     path: 'register',
     loadComponent: () => import('./register/register').then((m) => m.Register),
     title: 'Register',
@@ -19,6 +20,11 @@ export const AUTH_ROUTES: Routes = [
     path: 'reset-password',
     loadComponent: () => import('./reset-password/reset-password').then((m) => m.ResetPassword),
     title: 'Reset Password',
+  },
+  {
+    path: 'verify-account',
+    loadComponent: () => import('./verify-account/verify-account').then((m) => m.VerifyAccount),
+    title: 'Verify Account',
   },
   {
     path: '**',
--- src/app/features/auth/verify-account/verify-account.ts
+++ src/app/features/auth/verify-account/verify-account.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RefreshCw, Mail, LucideAngularModule, ArrowLeft, CircleCheck } from 'lucide-angular';
+import { ButtonModule } from 'primeng/button';
+import { InputOtpModule } from 'primeng/inputotp';
 
 @Component({
   selector: 'app-verify-account',
-  imports: [],
+  imports: [LucideAngularModule, FormsModule, InputOtpModule, ButtonModule],
   templateUrl: './verify-account.html',
   styleUrl: './verify-account.css',
 })
 export class VerifyAccount {
+  isLoading = signal(false);
+  isVerified = signal(false);
+  token = signal<string | null>('123');
+  email = signal<string | null>(null);
+  router = inject(Router);
 
+  icons = {
+    RefreshCw,
+    Mail,
+    ArrowLeft,
+    CircleCheck,
+  };
+
+  timer = signal(0);
+
+  otp = signal('');
+  otpFilled = computed(() =>
+    this.otp()
+      .split('')
+      .some((digit) => !digit),
+  );
+
+  handleResend() {}
+
+  onNavigate(path: string) {
+    this.router.navigate([path]);
+  }
 }
